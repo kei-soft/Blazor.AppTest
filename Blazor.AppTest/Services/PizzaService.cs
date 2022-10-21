13b181c80fd736f1fcb0b5fff1604bc3a73d7cad
준 강 <junijuniya@naver.com>
2022-10-21 23:33:55 +0900

    PizzaService

--- Blazor.AppTest/Services/PizzaService.cs
+++ Blazor.AppTest/Services/PizzaService.cs
@@ -6,12 +6,12 @@ namespace Blazor.AppTest.Services
     {
         public Task<PizzaItem[]> GetPizzasAsync()
         {
+            // Get DB Data
             PizzaItem[] pizzaItems = new PizzaItem[3];
             pizzaItems[0] = new PizzaItem() { PizzaId = 1, Name = "마스터 트리플 코드", Description = "디저트로 즐기는 피자", Price = 28000, Vegan = true, Vegetarian = true };
             pizzaItems[1] = new PizzaItem() { PizzaId = 2, Name = "치즈 퐁듀 파이어 미트", Description = "씨푸드 퐁듀 피자의 귀환", Price = 29000, Vegan = true, Vegetarian = true };
             pizzaItems[2] = new PizzaItem() { PizzaId = 3, Name = "블로버스터4", Description = "4개 도시의 프리미엄 요리를 품은 피자", Price = 29000, Vegan = true, Vegetarian = true };
 
-            // Call your data access technology here
             return Task.FromResult<PizzaItem[]>(pizzaItems);
         }
     }
