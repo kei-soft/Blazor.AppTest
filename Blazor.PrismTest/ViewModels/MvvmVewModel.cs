fb083f03eaac7e8c805108be7c644c2d16c08887
준 강 <junijuniya@naver.com>
2022-11-12 20:21:41 +0900

    ViewModel Subscribe 처리추가

--- Blazor.PrismTest/ViewModels/MvvmVewModel.cs
+++ Blazor.PrismTest/ViewModels/MvvmVewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Commands;
+﻿using Blazor.PrismTest.Events;
+
+using Prism.Commands;
 using Prism.Events;
 
 namespace Blazor.PrismTest.ViewModels
@@ -6,16 +8,23 @@ namespace Blazor.PrismTest.ViewModels
     public class MvvmVewModel
     {
         public int Count { get; set; }
-        public DelegateCommand MyCommand { get; set; }
+        public string? Message { get; set; }
+        public DelegateCommand? MyCommand { get; set; }
 
         public MvvmVewModel(IEventAggregator ea)
         {
-
+            ea.GetEvent<TestEvent>().Subscribe(TestMethod);
         }
 
         public void IncrementCount()
         {
             Count += 1;
+        }
+
+        private void TestMethod(string arg)
+        {
+            // Publish 호출시 처리
+            Message = "Call Method";
         }
     }
 }
