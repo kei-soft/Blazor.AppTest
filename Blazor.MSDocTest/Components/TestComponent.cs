aac9475d21a12de9f683f2d86c8aa868cc1c87d6
준 강 <junijuniya@naver.com>
2022-12-26 22:16:11 +0900

    Expander, component, renderfragment,

---
+++ Blazor.MSDocTest/Components/TestComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.AppTest.Components
+{
+    public class TestComponent : ComponentBase
+    {
+        public string? ButtonText;
+        public int Value = 0;
+        protected override Task OnInitializedAsync()
+        {
+            ButtonText = "Increment";
+
+            return base.OnInitializedAsync();
+        }
+
+        public void IncrementCount()
+        {
+            this.Value += 1;
+
+            StateHasChanged();
+        }
+    }
+}
