08fae56167c05a0e25969511cacea48f0db9cdc9
준 강 <junijuniya@naver.com>
2022-12-27 23:57:02 +0900

    Blazor.BlazorWorkerTest추가

---
+++ Blazor.BlazorWorkerTest/Services/TestService.cs
@@ -0,0 +1,21 @@
+﻿namespace Blazor.BlazorWorkerTest.Services
+{
+    public class TestService
+    {
+        public int TestMethod()
+        {
+            int i = 0;
+            Console.WriteLine("Starting long running task");
+            DateTime endTime = DateTime.UtcNow.AddSeconds(10);
+            while (DateTime.UtcNow < endTime)
+            {
+                i++;
+                Console.WriteLine("Task.Delay(100) - " + i.ToString());
+                Task.Delay(1000);
+            }
+            Console.WriteLine("Finished long running task");
+
+            return i;
+        }
+    }
+}
