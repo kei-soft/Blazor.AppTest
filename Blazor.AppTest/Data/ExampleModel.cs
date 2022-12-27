f8f05396a41b663b7f3557b3b53e1e0f2d52aa6c
준 강 <junijuniya@naver.com>
2022-12-27 22:32:13 +0900

    ValidationTest

---
+++ Blazor.AppTest/Data/ExampleModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Blazor.AppTest.Data;
+
+public class ExampleModel
+{
+    [Required]
+    [StringLength(10, ErrorMessage = "Name is too long.")]
+    public string? Name { get; set; }
+    [Required]
+    public int? Age { get; set; }
+    public Gender Gender { get; set; } = Gender.Male;
+}
