e4c70230275c253f3ecadb1c10069e26827d14be
kjun <keisoft.kjun@gmail.com>
2022-12-29 12:11:58 +0900

    ChangeableRequired

---
+++ Blazor.AppTest/Common/ChangeableRequired.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.AppTest.Common
+{
+    public class ChangeableRequired : RequiredAttribute
+    {
+        public bool Disabled { get; set; }
+
+        public override bool IsValid(object value)
+        {
+            if (Disabled)
+            {
+                return true;
+            }
+
+            return base.IsValid(value);
+        }
+    }
+}
