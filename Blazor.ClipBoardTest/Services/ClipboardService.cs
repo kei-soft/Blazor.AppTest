4c6de9695afcc5ffaa765532a9578acc277c7a9c
kjun <keisoft.kjun@gmail.com>
2023-07-27 00:06:55 +0900

    Blazor.ClipBoardTest 추가

---
+++ Blazor.ClipBoardTest/Services/ClipboardService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+
+namespace Blazor.ClipBoardTest.Services
+{
+    public class ClipboardService
+    {
+        private readonly IJSRuntime jsRuntime;
+
+        public ClipboardService(IJSRuntime jsRuntime)
+        {
+            this.jsRuntime = jsRuntime;
+        }
+
+        public ValueTask<string> ReadTextAsync()
+        {
+            return jsRuntime.InvokeAsync<string>("navigator.clipboard.readText");
+        }
+
+        public ValueTask WriteTextAsync(string text)
+        {
+            return jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+        }
+    }
+}
