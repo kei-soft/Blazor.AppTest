f375a04fdb402dbf7ee1e877feb8be129d8a9725
kjun <keisoft.kjun@gmail.com>
2023-07-28 16:51:25 +0900

    Blazor.EventCallBackTest 추가

---
+++ Blazor.EventCallBackTest/Components/ItemComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.EventCallBackTest.Components
+{
+    public partial class ItemComponent : ComponentBase
+    {
+        protected bool IsSelected { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> OnSelection { get; set; }
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        protected async Task CheckBoxChanged(ChangeEventArgs e)
+        {
+            IsSelected = (bool)(e.Value ?? false);
+            await OnSelection.InvokeAsync(IsSelected);
+        }
+    }
+}
---
+++ Blazor.EventCallBackTest/Components/ItemListComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.EventCallBackTest.Components
+{
+    public partial class ItemListComponent : ComponentBase
+    {
+        public List<string> DataList { get; set; } = new List<string> { "AA", "BB", "CC" };
+
+        protected int SelectedCount { get; set; } = 0;
+
+        protected void SelectionChanged(bool isSelected)
+        {
+            if (isSelected)
+            {
+                SelectedCount++;
+            }
+            else
+            {
+                SelectedCount--;
+            }
+        }
+    }
+}
